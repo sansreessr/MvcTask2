87afa796582b198cfda8bb5ec655048dba176d9d
sansreessr <sreenathsankarssr@gmail.com>
2019-09-25 17:58:07 +0530

    first commit

---
+++ src/main/java/com/stackroute/UserController.java
@@ -0,0 +1,25 @@
+package com.stackroute;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String user(Model model){
+        return "user";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String display(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Model model) {
+        User user = new User(username,password);
+        model.addAttribute("user",user.getUsername());
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/user.jsp
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    <h2>Spring Mvc App</h2>
+    <form id="login" action="login" method="post">
+      <label> Enter UserName: </label>
+        <input type="text" name="username"><br>
+      <label> Enter Password: </label>
+        <input type="text" name="password"><br>
+      <button type="submit"> Submit </button>
+    </form>
+  </body>
+</html>
